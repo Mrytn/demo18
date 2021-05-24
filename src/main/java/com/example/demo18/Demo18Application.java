32a1f79236d2500a576e4dfffb603d9b750b3d0b
PeiDong Yan <31368606+Mrytn@users.noreply.github.com>
2021-05-24 10:51:59 +0800

    10

--- src/main/java/com/example/demo18/Demo18Application.java
+++ src/main/java/com/example/demo18/Demo18Application.java
@@ -26,6 +26,7 @@ public class Demo18Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Demo18Application.class, args);
+        System.out.println(100);
         System.out.println(999);
         SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
 //        String date = sdf.format(LocalDate.now().toString());
